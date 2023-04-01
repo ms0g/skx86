eed5a9ac69b856b47e9d847fd851a1ff55a093dc
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2023-04-01 16:11:41 +0300

    feat(Boot): switched to protected mode

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,5 +1,8 @@
-org 0
+org  0x7c00
 bits 16
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
 
 ; faked bios parameter block since running on real hardware.
 ; https://wiki.osdev.org/FAT#BPB_.28BIOS_Parameter_Block.29
@@ -11,37 +14,65 @@ times 33 db 0
 
 start:
      jmp 0x7c0:init
+
 init:
      cli ; clear interrupts
-     mov ax, 0x7c0
+     mov ax, 0x00
      mov ds, ax
      mov es, ax
-     mov ax, 0x00
      mov ss, ax
      mov sp, 0x7c00
      sti ; enable interrupts
 
-     mov si, message
-     call print
-     jmp $
+.load_protected:
+     cli
+     lgdt[gdt_descriptor]
+     mov eax, cr0
+     or eax, 0x1
+     mov cr0, eax
+     jmp CODE_SEG:load32
 
-print:
-     mov bx, 0
-.loop:
-     lodsb
-     cmp al, 0
-     je .done
-     call print_char
-     jmp .loop
-.done:
-     ret
+; GDT
+gdt_start:
+gdt_null:
+    dd 0x0
+    dd 0x0
 
-print_char:
-     mov ah, 0eh
-     int 0x10
-     ret
+; offset 0x8
+gdt_code:     ; CS SHOULD POINT TO THIS
+    dw 0xffff ; Segment limit first 0-15 bits
+    dw 0      ; Base first 0-15 bits
+    db 0      ; Base 16-23 bits
+    db 0x9a   ; Access byte
+    db 11001111b ; High 4 bit flags and the low 4 bit flags
+    db 0        ; Base 24-31 bits
 
-message: db 'Hello World!', 0
+; offset 0x10
+gdt_data:      ; DS, SS, ES, FS, GS
+    dw 0xffff ; Segment limit first 0-15 bits
+    dw 0      ; Base first 0-15 bits
+    db 0      ; Base 16-23 bits
+    db 0x92   ; Access byte
+    db 11001111b ; High 4 bit flags and the low 4 bit flags
+    db 0        ; Base 24-31 bits
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start-1
+    dd gdt_start
+
+[bits 32]
+load32:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov ebp, 0x00200000
+    mov esp, ebp
+    jmp $
 
 times 510-($ - $$) db 0
 dw 0xAA55
