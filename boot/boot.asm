c7b4b29c770a4e71c44ae59b6a9933b0039ea590
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2023-04-02 05:33:30 +0300

    feat(Boot): enabled A20 line

--- boot/boot.asm
+++ boot/boot.asm
@@ -72,6 +72,12 @@ load32:
     mov ss, ax
     mov ebp, 0x00200000
     mov esp, ebp
+
+    ; Enable A20 line
+    in al, 0x92
+    or al, 2
+    out 0x92, al
+    
     jmp $
 
 times 510-($ - $$) db 0
