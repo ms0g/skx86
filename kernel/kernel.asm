03e6c0d67300caaec911128c1c9d86ff77237070
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2023-04-07 07:06:16 +0300

    feat(kernel): added c file, first hello world

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -1,5 +1,6 @@
 [bits 32]
 global _start
+extern kernel_main
 
 CODE_SEG equ 0x08
 DATA_SEG equ 0x10
@@ -18,6 +19,7 @@ _start:
     in al, 0x92
     or al, 2
     out 0x92, al
+    call kernel_main
     
     jmp $
 
